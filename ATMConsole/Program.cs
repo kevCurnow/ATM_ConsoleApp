12f40d1d1ba23b0df2559f5db8b297b6b9bfb9e9
Kevin Curnow <kevinscurnow@gmail.com>
2017-11-21 10:38:19 -0500

    initial commit for new project

---
+++ ATMConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
